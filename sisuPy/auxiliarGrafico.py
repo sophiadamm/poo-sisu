a1363e97ab79856af390b294e214ec9e917091d7
raissacavalcante <raissa.cavalcante@dcomp.ufs.br>
2025-08-31 15:14:39 -0300

    grafico barra de notas

--- sisuPy/auxiliarGrafico.py
+++ sisuPy/auxiliarGrafico.py
@@ -1,9 +1,43 @@
 
 
-class auxiliarGrafico:
+class AuxiliarGrafico:
     def __init__(self, nome_campus):
         self.nome_campus = nome_campus
-        self.notas_por_ano = {}
-    
+        self.notas_por_ano = {}   # {ano: menor_nota}
+
     def adicionar_nota(self, ano, nota):
-        self.notas_por_ano[ano] = nota
+        """Armazena a menor nota para o ano.
+           Converte ano -> int e nota -> float por segurança."""
+        ano = int(ano)
+        nota = float(nota)
+        atual = self.notas_por_ano.get(ano)
+        if atual is None or nota < atual:
+            self.notas_por_ano[ano] = nota
+
+    def anos_ordenados(self):
+        return sorted(self.notas_por_ano.keys())
+
+    def itens_ordenados(self):
+        """Retorna lista de tuplas (ano, nota) ordenadas por ano."""
+        return [(ano, self.notas_por_ano[ano]) for ano in self.anos_ordenados()]
+
+    def __repr__(self):
+        return f"<AuxiliarGrafico {self.nome_campus} {self.notas_por_ano}>"
+    
+    @staticmethod
+    def agrupar_dados_por_campus(dados, curso_selecionado=None):
+        mapas = {}
+        for candidato in dados:
+            # se quiser filtrar por curso:
+            if curso_selecionado is not None and candidato.curso() != curso_selecionado:
+                continue
+
+            campus = candidato.campus()
+            ano = int(candidato.ano())
+            media = float(candidato.media())
+
+            if campus not in mapas:
+                mapas[campus] = AuxiliarGrafico(campus)
+            mapas[campus].adicionar_nota(ano, media)
+
+        return mapas
---
+++ sisuPy/graficobarra.py
@@ -0,0 +1,82 @@
+from collections import Counter
+from typing import List, Set
+from PyQt5 import QtWidgets
+from PyQt5 import QtChart
+from PyQt5 import QtCore
+from PyQt5 import QtGui
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter
+from PyQt5.QtWidgets import QWidget
+
+class GraficoBarra(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.resize(1000, 600)
+        self.widget = QtWidgets.QWidget(self)
+        self.widget.setStyleSheet('background-color: linegreen')
+        self.vertical_layout = QtWidgets.QVBoxLayout(self.widget)
+        self.set0 = QtChart.QBarSet('menor_nota')
+        self.set1 = QtChart.QBarSet('valor_medio')
+        self.set2 = QtChart.QBarSet('maior_nota')
+
+    def setDados(self, dados: List):
+        self.dados = dados
+        maior_nota = {}
+        menor_nota = {}
+        valor_medio = {}
+        soma_notas = {}
+        contagem_candidatos = {}
+
+        for c in dados:
+            ano = c.ano()
+            media = c.media()
+            if ano not in maior_nota or media > maior_nota[ano]:
+                maior_nota[ano] = media
+            
+            if ano not in menor_nota or media < menor_nota:
+                menor_nota[ano] = media
+            
+            soma_notas[ano] = soma_notas.get(ano, 0.0) + media
+            contagem_candidatos[ano] = contagem_candidatos.get(ano, 0) + 1
+        
+        for ano in soma_notas:
+            if contagem_candidatos[ano] > 0:
+                media_calculada = soma_notas[ano] / contagem_candidatos[ano]
+                valor_medio[ano] = media_calculada
+        
+        self.set0 = QtChart.QBarSet('menor_nota')
+        self.set1 = QtChart.QBarSet('valor_medio')
+        self.set2 = QtChart.QBarSet('maior_nota')
+
+        self.set0.append(menor_nota.values())
+        self.set1.append(valor_medio.values())
+        self.set2.append(maior_nota.values())
+
+        self.bar_series = QtChart.QBarSeries()
+        self.bar_series.append(self.set0)
+        self.bar_series.append(self.set1)
+        self.bar_series.append(self.set2)
+
+        self.chart = QtChart.QChart()
+        self.chart.addSeries(self.bar_series)
+        self.chart.setTitle("Gráfico com a nota de corte, nota média e nota máximo do curso no decorrer dos anos")
+
+        self.categories = ['2019', '2020', '2021', '2022', '2023', '2024', '2025']
+        self.x_axis = QtChart.QBarCategoryAxis()
+        self.x_axis.append(self.categories)
+        self.chart.setAxisX(self.x_axis, self.bar_series)
+
+        self.y_axis = QtChart.QValueAxis()
+        self.chart.setAxisY(self.y_axis, self.bar_series)
+        self.y_axis.setRange(0, 1000)
+
+        self.chart.legend().setVisible(True)
+        self.chart.legend().setAlignment(QtCore.Qt.AlignBottom)
+        self.chart.axisX().setTitleText('Ano')
+        self.chart.axisY().setTitleText('Nota')
+        self.chart.setAnimationOptions(QtChart.QChart.SeriesAnimations)
+
+        self.chart_view = QtChart.QChartView(self.chart)
+        self.chart_view.setRenderHint(QtGui.QPainter.Antialiasing)
+
+        self.vertical_layout.addWidget(self.chart_view)
